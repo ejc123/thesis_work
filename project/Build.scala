e4b93ee30eb1669ebde113878372d7901c4e07cc
Eric J. Christeson <Eric.J.Christeson@gmail.com>
2013-09-11 14:48:23 -0500

    Fix up for stand-alone operation.

--- project/Build.scala
+++ project/Build.scala
@@ -1,47 +1,68 @@
 import sbt._
 import sbt.Keys._
+import sbtassembly.Plugin._
+import AssemblyKeys._
 
 object MyBuild extends Build {
 
-  lazy val project = Project("root", file(".")) settings(
-    //organization := "org.sample.demo",
+  lazy val sharedSettings = Defaults.defaultSettings ++ Seq(
+      //organization := "org.sample.demo",
 
-    name := "geotrellis-example",
+      name := "geotrellis-example",
 
-    scalaVersion := "2.10.2",
+      scalaVersion := "2.10.2",
 
-    scalacOptions ++= Seq("-deprecation",
-                          "-unchecked",
-                          "-optimize",
-                          "-feature"),
+      scalacOptions ++= Seq("-deprecation",
+        "-unchecked",
+        "-optimize",
+        "-feature"),
 
-    parallelExecution := false,
+      parallelExecution := false,
 
-    mainClass in (Compile, run) := Some("geotrellis.rest.WebRunner"),
+    mainClass in run := Some("geotrellis.rest.WebRunner"),
 
-    javaOptions in run += "-Xmx8G",
+      javaOptions in run += "-Xmx8G",
 
-    libraryDependencies ++= Seq(
-      "junit" % "junit" % "4.5" % "test",
-      "org.eclipse.jetty" % "jetty-webapp" % "8.1.0.RC4",
-      "com.sun.jersey" % "jersey-bundle" % "1.11",
-      "com.azavea.geotrellis" %% "geotrellis-geotools" % "0.9.0-SNAPSHOT",
-      "com.azavea.geotrellis" %% "geotrellis-server" % "0.9.0-SNAPSHOT",
-      "com.azavea.geotrellis" %% "geotrellis-tasks" % "0.9.0-SNAPSHOT",
-      "com.azavea.geotrellis" %% "geotrellis" % "0.9.0-SNAPSHOT"
-    ),
+      libraryDependencies ++= Seq(
+        "com.azavea.geotrellis" %% "geotrellis-geotools" % "0.9.0-SNAPSHOT" % "compile",
+        "com.azavea.geotrellis" %% "geotrellis-tasks" % "0.9.0-SNAPSHOT" % "compile",
+        "com.azavea.geotrellis" %% "geotrellis" % "0.9.0-SNAPSHOT" % "compile",
+        "com.azavea.geotrellis" %% "geotrellis-server" % "0.9.0-SNAPSHOT" % "compile",
+        "junit" % "junit" % "4.5" % "test",
+        "org.eclipse.jetty" % "jetty-webapp" % "8.1.0.RC4",
+        "com.sun.jersey" % "jersey-bundle" % "1.11",
+        "org.scalatest"  %% "scalatest"  % "1.9.1" % "test"
+      ),
 
-    resolvers ++= Seq(
-      "opengeo" at "http://repo.opengeo.org/",
-      "NL4J Repository" at "http://nativelibs4java.sourceforge.net/maven/",
-      "maven2 dev repository" at "http://download.java.net/maven/2",
-      "Scala Test" at "http://www.scala-tools.org/repo-reloases/",
-      "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/",
-      "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository",
-      "sonatypeSnapshots" at "http://oss.sonatype.org/content/repositories/snapshots"
-    ),
+      resolvers ++= Seq(
+        "opengeo" at "http://repo.opengeo.org/",
+        "NL4J Repository" at "http://nativelibs4java.sourceforge.net/maven/",
+        "maven2 dev repository" at "http://download.java.net/maven/2",
+        "Scala Test" at "http://www.scala-tools.org/repo-reloases/",
+        "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/",
+        "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository",
+        "sonatypeSnapshots" at "http://oss.sonatype.org/content/repositories/snapshots"
+      ),
 
-    // enable forking in run
-    fork in run := true
+      // enable forking in run
+      fork in run := true
+    )
+
+    lazy val localAssembly = assemblySettings ++ Seq(
+        mainClass in assembly := Some("geotrellis.rest.WebRunner"),
+        mergeStrategy in assembly <<= (mergeStrategy in assembly) { (old) =>
+          {
+            case x if x startsWith "org/apache/commons/collections" => MergeStrategy.first
+            case x if x startsWith "org/postgresql" => MergeStrategy.filterDistinctLines
+            case PathList("META-INF", "registryFile.jai") => MergeStrategy.filterDistinctLines
+            case x => old(x)
+          }
+        }
+    )
+
+  lazy val project = Project(
+       id = "example",
+       base = file("."),
+       settings = sharedSettings ++ localAssembly
   )
 }
--- src/main/scala/net/christeson/geotrellis/template/rest/TemplateResource.scala
+++ src/main/scala/net/christeson/geotrellis/template/rest/TemplateResource.scala
@@ -16,7 +16,7 @@ import geotrellis.Literal
 import geotrellis.process.Error
 import geotrellis.process.Complete
 
-import scala.math._
+//import scala.math._
 import geotrellis.statistics.Histogram
 import geotrellis.raster.op.local.{Round, Multiply}
 
@@ -24,7 +24,7 @@ import geotrellis.raster.op.local.{Round, Multiply}
  * Simple hello world rest service that responds to "/hello"
  */
 object Demo {
-  val server = Server("demo", "src/main/resources/catalog.json")
+  val server = Server("demo", "catalog.json")
 
   def infoPage(cols: Int, rows: Int, ms: Long, url: String, tree: String) = """
   <html>
@@ -77,40 +77,26 @@ object response {
 class Draw {
   @GET
   @Path("/{map}")
-  def get(@DefaultValue("0414") @PathParam("map") map: String,
+  def get(@DefaultValue("0625") @PathParam("map") map: String,
           @Context req: HttpServletRequest) = {
-    val palette = "2791c3,5da1ca,83B2D1,A8C5D8,CCDBE0,E9D3C1,DCAD92,D08B6C,66E4B,BD4E2E"
-    val numColors = "10"
+    // val palette = "2791c3,5da1ca,83B2D1,A8C5D8,CCDBE0,E9D3C1,DCAD92,D08B6C,66E4B,BD4E2E"
+    // val numColors = "10"
     val format = map match {
-      case "info" => "0414"
+      case "info" => "0625"
       case _ => map
     }
-    val rasterOp: Op[Raster] = io.LoadRaster("test_out_2007_" + format)
-    //val rasterOp: Op[Raster] = io.LoadRaster("SBN_farm_mkt")
-    println("test_out_2007_" + format)
+    val rasterOp: Op[Raster] = io.LoadRaster("ltm7_clean_2007_" + format)
+    println("ltm7_clean_2007_" + format)
 
-
-    val raster2 = Multiply(rasterOp,1000.0)
-    val histogramOp = stat.GetHistogram(raster2)
-    val breaksOp = stat.GetColorBreaks(histogramOp, Literal(BlueToOrange.toArray))
-    val pngOp = io.RenderPng(raster2, breaksOp, histogramOp, Literal(2))
+    //val raster2 = Round(Multiply(rasterOp,1000.0))
+    val histogramOp = stat.GetHistogram(rasterOp)
+    val breaksOp = stat.GetColorBreaks(histogramOp, Literal(ClassificationBoldLandUse.toArray))
+    val pngOp = io.RenderPng(rasterOp, breaksOp, histogramOp, Literal(2))
     // val pngOp = io.SimpleRenderPng(rasterOp,BlueToRed)
 
     /*
     val img = Demo.server.run(pngOp)
     response("image/png")(img)
-    */
-    var cols = 0
-    var rows = 0
-    Demo.server.getResult(RasterColRows(raster2)) match {
-      case Complete(minmax,timing) => {
-        cols = minmax._1
-        rows = minmax._2
-      }
-      case _ => println("An Error Occurred")
-
-    }
-/*
     Demo.server.getResult(hist(histogramOp)) match {
       case Complete(foo,_) => {
         for {
@@ -127,7 +113,14 @@ class Draw {
             val ms = h.elapsedTime
             val url = "/gt/draw/" + format
             println(url)
-            val html = Demo.infoPage(cols, rows, ms, url, h.toPretty)
+            val (cols,rows): (Int,Int) = Demo.server.getResult(RasterColRows(rasterOp)) match {
+              case Complete(minmax,timing) => {
+                minmax
+              }
+              case _ => println("An Error Occurred"); (0,0)
+
+            }
+            val html = Demo.infoPage(cols, rows, ms, url, h.toPretty())
             response("text/html")(html)
           }
           case _ => Response.ok(img).`type`("image/png").build()
@@ -136,5 +129,20 @@ class Draw {
       }
       case Error(msg, trace) => Response.ok("failed: %s\ntrace:\n%s".format(msg, trace)).build()
     }
+/*
+    Demo.server.getResult(rasterOp) match {
+      case Complete(foo,h) => {
+        val ms = h.elapsedTime
+        val url = "/gt/"
+        val (cols,rows) = Demo.server.getResult(RasterColRows(rasterOp)) match {
+          case Complete(colrows,timing) =>  colrows
+          case _ => println("Error"); (0,0)
+        }
+        val html = Demo.infoPage(cols,rows,ms,url,h.toPretty)
+        response("text/html")(html)
+      }
+      case Error(msg, trace) => Response.ok("failed: %s\ntrace:\n%s".format(msg, trace)).build()
+    }
+    */
   }
 }
