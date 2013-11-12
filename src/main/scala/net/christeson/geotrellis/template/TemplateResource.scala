f1f0645472ff043ce893583d1c89cbe234ef7931
Eric J. Christeson <Eric.J.Christeson@gmail.com>
2013-11-12 00:33:05 -0600

    Refactor

--- src/main/scala/net/christeson/geotrellis/template/TemplateResource.scala
+++ src/main/scala/net/christeson/geotrellis/template/TemplateResource.scala
@@ -23,7 +23,7 @@ import geotrellis.raster.op.zonal.summary.ZonalSummaryOpMethods
 import RasterLoader._
 
 object Demo {
-  val server = Server("demo","src/main/resources/catalog.json")
+  val server = Server("demo", "src/main/resources/catalog.json")
 }
 
 object RunMe {
@@ -33,27 +33,18 @@ object RunMe {
     import scala.util.Random
 
     implicit val codec = scalax.io.Codec.UTF8
-    // println("Loading geometry file")
-    val startNanos = System.nanoTime()
+    var startNanos = System.nanoTime()
     val resource = Resource.fromURL(featurePath).chars
     val geoJson = resource.mkString
     val geoms = Demo.server.run(io.LoadGeoJson(geoJson))
-    val (valid, invalid) = geoms.partition(_.geom.isValid)
-    val stopNanos = System.nanoTime()
-    val diff = stopNanos - startNanos
-    println(s"Load Geometry file took: ${diff / 1000000} ms")
+    val valid = geoms.filter(node => node.geom.isValid && node.geom.getGeometryType == "Polygon")
+    var stopNanos = System.nanoTime()
+    println(s"Load Geometry file took: ${(stopNanos - startNanos) / 1000000} ms")
 
-    val tenPercent = Random.shuffle(valid.toList).take((valid.length * .10).toInt)
-    // val tenPercent = valid.toList.take((valid.length * .10).toInt).par
-    // println("tileset loaded")
+    val tenPercent = Random.shuffle(valid.toList).take((valid.length * .30).toInt).par
 
     try {
-      /*
-      val results = for {
-      //g <- tenPercent.filter(_.geom.getGeometryType == "Polygon").take(3)
-      g <- tenPercent.filter(_.geom.getGeometryType == "Polygon").take(20)
-         date <- dates
-      } yield {
+      val results = tenPercent.flatMap(g => dates.map(date => {
         val reproj = Transformer.transform(g, Projections.LongLat, Projections.RRVUTM)
         val polygon = Polygon(reproj.geom, 0)
         val id = reproj.data.get.get("IND").getDoubleValue.toInt
@@ -64,41 +55,19 @@ object RunMe {
         val meanOp = tileSet.zonalMean(polygon)
         Demo.server.getSource(meanOp) match {
           case Complete(result, _) => {
-            (id,(lat,lon),result)
+            (id, (lat, lon), result)
           }
-          case _ => (-1,(0,0),Double.NaN)
-        }
-      }
-      */
-      val results = tenPercent.filter(_.geom.getGeometryType == "Polygon").flatMap(g => dates.map(date =>
-      {
-        val reproj = Transformer.transform(g, Projections.LongLat, Projections.RRVUTM)
-        val polygon = Polygon(reproj.geom, 0)
-        val id = reproj.data.get.get("IND").getDoubleValue.toInt
-        val lat = reproj.data.get.get("LATITUDE").getDoubleValue
-        val lon = reproj.data.get.get("LONGITUDE").getDoubleValue
-        val tileSet = RasterLoader.load(s"ltm5_2007_${date}_clean")
-        // val tileSet = RasterLoader.load(s"ltm5_2007_0921_clean")
-        val meanOp = tileSet.zonalMean(polygon)
-        Demo.server.getSource(meanOp) match {
-          case Complete(result, _) => {
-            (id,(lat,lon),result)
-          }
-          case _ => (-1,(0,0),Double.NaN)
+          case _ => (-1, (0, 0), Double.NaN)
         }
       }
 
-      ) )
-      // val filtered = results.groupBy(a => a._1 ).map(a => a._1 -> a._2.map(_._2).filter(_ != geotrellis.NODATA ))
+      ))
       val filtered = results.filter(a => !isNaN(a._3))
-      val grouped = filtered.groupBy{case (a,b,_) => (a,b)}.mapValues(a => a.map(_._3).toList)
+      val grouped = filtered.groupBy { case (a, b, _) => (a, b) }.mapValues(a => a.map(_._3).toList)
       grouped.foreach(println(_))
       println(s"Results length ${results.length}")
       println(s"Filtered length ${filtered.size}")
       println(s"Grouped length ${grouped.size}")
-      // println(s"Total time for Mean: ${results.foldLeft(0L)((a,b) => a + b._4)} ms")
-      //  filtered.map(m => {println(s"Key: ${m._1}"); println("   Values:"); m._2.map(b => println(s"    $b")) })
-
     }
     finally {
       Demo.server.shutdown()
