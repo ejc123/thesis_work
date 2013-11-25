4b1a16b59d80da6a7298c1eae3ba5ec0a0788887
Eric J. Christeson <Eric.J.Christeson@gmail.com>
2013-11-25 16:38:33 -0600

    Remove commented code form Transformer.

--- src/main/scala/net/christeson/geotrellis/template/Transformer.scala
+++ src/main/scala/net/christeson/geotrellis/template/Transformer.scala
@@ -13,28 +13,6 @@ import com.vividsolutions.jts.{ geom => jts }
 
 
 object Transformer {
-  /*
-  private val transformCache:mutable.Map[(Crs,Crs),MathTransform] =
-    new mutable.HashMap[(Crs,Crs),MathTransform]()
-  
-  def cacheTransform(crs1:Crs,crs2:Crs) = {
-    transformCache((crs1,crs2)) = CRS.findMathTransform(crs1,crs2,true)
-  }
-
-  private def initCache() = {
-    cacheTransform(Projections.LongLat,Projections.RRVUTM)
-    cacheTransform(Projections.RRVUTM,Projections.LongLat)
-  }
-
-  initCache()
-
-  def transform[D](feature:Geometry[D],fromCRS:Crs,toCRS:Crs):Geometry[D] = {
-    if(!transformCache.contains((fromCRS,toCRS))) { cacheTransform(fromCRS,toCRS) }
-    feature.mapGeom( geom => 
-      JTS.transform(feature.geom, transformCache((fromCRS,toCRS)))
-    )
-  }
-  */
   def transform[D](feature:Geometry[D],fromCRS:Crs,toCRS:Crs):Geometry[D] = {
     feature.mapGeom( geom =>
       JTS.transform(feature.geom,  CRS.findMathTransform(fromCRS,toCRS,true))
