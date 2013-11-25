34e39bed23f0c4a294544794a6fc3378aac8c58a
Eric J. Christeson <Eric.J.Christeson@gmail.com>
2013-11-25 16:26:29 -0600

    Fix up file output   sort by id, convert to seq list before output, keep NaN values

--- src/main/scala/net/christeson/geotrellis/template/RasterLoader.scala
+++ src/main/scala/net/christeson/geotrellis/template/RasterLoader.scala
@@ -5,26 +5,10 @@ import geotrellis.source.RasterSource
 import scala.collection.mutable
 
 object RasterLoader {
-  val dates = List("0414","0430","0516","0601","0617","0703","0719","0804","0820","0905","0921")
+  val dates = List("0414","0430","0516","0601","0617","0703","0719","0804","0820","0905","0921").par
   val tilePath = "/home/ejc/geotrellis/data/tiled64"
-  private val loadCache:mutable.Map[String,RasterSource] =
-    new mutable.HashMap[String,RasterSource]()
-
-  def loadRaster(name: String) = {
-    loadCache(name) =  Demo.server.run(RasterSource(name))
-  }
-
-  private def initCache() = dates.map(a => loadRaster(s"$tilePath/ltm5_2007_${a}_clean.json"))
-
-
-  // val startNanos = System.nanoTime()
-  // initCache()
-  // val stopNanos = System.nanoTime()
-  // val diff = stopNanos - startNanos
-  // println(s"Init raster cache took: ${diff / 1000000} ms")
 
   def load(name: String):RasterSource = {
-    if(!loadCache.contains(name)) { loadRaster(name) }
-    loadCache(name)
+    Demo.server.run(RasterSource(name))
   }
 }
