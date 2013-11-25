620cea87a0317be1f5db94a953b7cb41ea8e5fef
Eric J. Christeson <Eric.J.Christeson@gmail.com>
2013-11-25 16:34:47 -0600

    Fix output up to work with model software.

--- src/main/scala/net/christeson/geotrellis/template/TemplateResource.scala
+++ src/main/scala/net/christeson/geotrellis/template/TemplateResource.scala
@@ -36,39 +36,96 @@ object RunMe {
     var startNanos = System.nanoTime()
     val resource = Resource.fromURL(featurePath).chars
     val geoJson = resource.mkString
-    val geoms = Demo.server.run(io.LoadGeoJson(geoJson))
+    val geoms = Demo.server.run(io.LoadGeoJson(geoJson)).par
     val valid = geoms.filter(node => node.geom.isValid && node.geom.getGeometryType == "Polygon")
     var stopNanos = System.nanoTime()
     println(s"Load Geometry file took: ${(stopNanos - startNanos) / 1000000} ms")
 
-    val tenPercent = Random.shuffle(valid.toList).take((valid.length * .30).toInt).par
+    val tenPercent = Random.shuffle(valid.toList).take((valid.length * .10).toInt)
 
     try {
-      val results = tenPercent.flatMap(g => {
-        val reproj = Transformer.transform(g, Projections.LongLat, Projections.RRVUTM)
-        val polygon = Polygon(reproj.geom, 0)
-        val id = reproj.data.get.get("IND").getDoubleValue.toInt
-        val lat = reproj.data.get.get("LATITUDE").getDoubleValue
-        val lon = reproj.data.get.get("LONGITUDE").getDoubleValue
-        dates.map(date => {
-          val tileSet = RasterLoader.load(s"ltm5_2007_${date}_clean")
-          val meanOp = tileSet.zonalMean(polygon)
-          Demo.server.getSource(meanOp) match {
-            case Complete(result, _) => {
-              (id, (lat, lon), result)
-            }
-            case _ => (-1, (0, 0), Double.NaN)
-          }
-        }
-        )
-      })
+      val results = tenPercent.flatMap {
+        g =>
+          dates.map {
+            date => {
+              val id = g.data.get.get("IND").getDoubleValue.toInt
+              val lat = g.data.get.get("LATITUDE").getDoubleValue
+              val lon = g.data.get.get("LONGITUDE").getDoubleValue
+              val reproj = Transformer.transform(g, Projections.LongLat, Projections.RRVUTM)
+              val polygon = Polygon(reproj.geom, 0)
+              val tileSet = RasterLoader.load(s"ltm5_2007_${date}_clean")
+              Demo.server.getSource(tileSet.zonalMean(polygon)) match {
+                case Complete(result, _) => {
+                  (id, lat, lon, result)
+                }
+                case _ => (-1, 0, 0, Double.NaN)
+              }
 
-      val filtered = results.filter(a => !isNaN(a._3))
-      val grouped = filtered.groupBy { case (a, b, _) => (a, b) }.mapValues(a => a.map(_._3).toList)
-      grouped.foreach(println(_))
-      println(s"Results length ${results.length}")
-      println(s"Filtered length ${filtered.size}")
-      println(s"Grouped length ${grouped.size}")
+            }
+          }
+      }
+      /*       val results = tenPercent.flatMap(g => {
+               val id = g.data.get.get("IND").getDoubleValue.toInt
+               val lat = g.data.get.get("LATITUDE").getDoubleValue
+               val lon = g.data.get.get("LONGITUDE").getDoubleValue
+               val reproj = Transformer.transform(g, Projections.LongLat, Projections.RRVUTM)
+               val polygon = Polygon(reproj.geom, 0)
+               dates.map(date => {
+                 val tileSet = RasterLoader.load(s"ltm5_2007_${date}_clean")
+                 Demo.server.getSource(tileSet.zonalMean(polygon)) match {
+                   case Complete(result, _) => {
+                     (id, lat, lon, result)
+                   }
+                   case _ => (-1, 0, 0, Double.NaN)
+                 }
+               }
+               )
+             }).filter(a => !isNaN(a._4)).groupBy {
+               case (a, b, c, _) => (a, b, c)
+             }.mapValues(b => b.map(_._4))
+       //
+       val results = dates.flatMap(date => {
+         val tileSet = RasterLoader.load(s"ltm5_2007_${date}_clean")
+         tenPercent.map(g => {
+           val reproj = Transformer.transform(g, Projections.LongLat, Projections.RRVUTM)
+           val polygon = Polygon(reproj.geom, 0)
+           val id = reproj.data.get.get("IND").getDoubleValue.toInt
+           val lat = reproj.data.get.get("LATITUDE").getDoubleValue
+           val lon = reproj.data.get.get("LONGITUDE").getDoubleValue
+           val meanOp = tileSet.zonalMean(polygon)
+           Demo.server.getSource(meanOp) match {
+             case Complete(result, _) => {
+               (id, lat, lon, result)
+             }
+             case _ => (-1, 0, 0, Double.NaN)
+           }
+         })
+       })
+       */
+      /*
+       .filter.(a => !isNaN(a._4)).groupBy {
+       case (a, b, c, _) => (a, b, c)
+     }.mapValues(b => b.map(_._4))
+
+*/
+      // val filtered = results.filter(a => !isNaN(a._3))
+      // val grouped = filtered.groupBy {
+      //   case (a, b, c, _) => (a, b, c)
+      // }.mapValues(a => a.map(_._4))
+      import java.io.PrintWriter
+      val output = new PrintWriter("/home/ejc/results.txt")
+      val filtered = results.filter(a => !isNaN(a._4)).groupBy {
+        case (a, b, c, _) => (a, b, c)
+      }.mapValues(b => b.map(_._4)).toList.sortBy(a => a._1._1).seq
+      filtered.foreach(a => {
+        output.print(s"${a._1._1},${a._1._2},${a._1._3}")
+        a._2.map(b => output.print(s",$b"))
+        output.println()
+      }
+      )
+      println(s"Results length ${results.size}")
+      // println(s"Filtered length ${filtered.size}")
+      // println(s"Grouped length ${grouped.size}")
     }
     finally {
       Demo.server.shutdown()
