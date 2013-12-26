752ff152d2cf5d8d97d89ce866db332847823351
Eric J. Christeson <Eric.J.Christeson@gmail.com>
2013-12-26 15:42:49 -0600

    Add Landsat7 settins   add options for range of years

--- src/main/scala/net/christeson/geotrellis/template/TemplateResource.scala
+++ src/main/scala/net/christeson/geotrellis/template/TemplateResource.scala
@@ -14,65 +14,76 @@ object Demo {
 }
 
 class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
-  val year = opt[Int](required = true)
+  val start = opt[Int](required = true)
+  val end = opt[Int](required = true)
   val store = opt[String](default = Some("tiled"), required = true)
-  val negative = opt[Boolean]()
+  val sat = opt[Int](required = true, default=Some(5))
   val prior = toggle(descrYes = "Use prior year for negative sample", descrNo = "Use next year for negative sample")
-  codependent(negative,prior)
-  validate (year) { a =>
-    if(a <=2011 && a >= 2007)  Right(Unit)
-    else Left("year must be between 2007 and 2011 inclusive")
+  val outputPath = opt[String](default = Some("/home/ejc"))
+
+  validate (sat) { a =>
+    if(a == 5 || a == 7) Right(Unit)
+    else Left("sat must be either 5 or 7")
   }
 
+  validate (sat,start) { (sa,st) =>
+    sa match {
+      case 5 => if(st<=2011 && st >= 2007)  Right(Unit)
+        else Left("year must be between 2007 and 2011 inclusive for landsat5")
+      case 7 =>  if(st <=2013 && st >= 2008)  Right(Unit)
+        else Left("year must be between 2008 and 2013 inclusive for landsat7")
+    }
+  }
 }
 
 object RunMe {
   def main(args: Array[String]): Unit = {
     val conf = new Conf(args)
 
-    val year = conf.year()
+    val start = conf.start()
+    val end = conf.end()
 
-    val feature_year = conf.negative() match {
-      case true => conf.prior() match {
-        case true => year - 1
-        case _ => year + 1
-      }
-      case _ => year
+    val prior = conf.prior() match {
+        case true => -1
+        case _ => 1
     }
+    val outputPath = conf.outputPath()
+    val sat = conf.sat()
 
-    val beets = conf.negative() match {
-      case true => "nonbeets"
-      case _ => "beets"
-    }
-
-    val beetfile = conf.negative() match {
-      case true => s"$beets$feature_year"
-      case _ => beets
-    }
-
-    val featurePath = s"file:///home/ejc/geotrellis/data/${feature_year}_field_boundary_cropped.geojson"
     import scalax.io.Resource
-
     implicit val codec = scalax.io.Codec.UTF8
-    var startNanos = System.nanoTime()
-    val resource = Resource.fromURL(featurePath).chars
-    val geoJson = resource.mkString
-    val geoms = Demo.server.get(io.LoadGeoJson(geoJson)).par
-    val valid = geoms.filter(node => node.geom.isValid && node.geom.getGeometryType == "Polygon")
-    var stopNanos = System.nanoTime()
-    println(s"Load Geometry file took: ${(stopNanos - startNanos) / 1000000} ms")
 
-    // import scala.util.Random
-    // val tenPercent = Random.shuffle(valid.toList).take((valid.length * .10).toInt)
+    import scala.util.Random
 
     try {
-     val results = valid.flatMap {g =>
-     // val results = tenPercent.flatMap {g =>
-       dates(year).map {date =>
+      for {
+        year <- start to end
+        feature_year <- (year + prior) match {
+          case a if (a < year) => year to a by -1
+          case a => year to a
+        }
+     } yield {
+        val beets = feature_year == year match {
+          case true => "beets"
+          case false => "nonbeets"
+        }
+        val beetfile = feature_year == year match {
+          case true => beets
+          case false => s"$beets$feature_year"
+        }
+        val featurePath = s"file:///home/ejc/geotrellis/data/${feature_year}_field_boundary_cropped.geojson"
+        val resource = Resource.fromURL(featurePath).chars
+        val geoJson = resource.mkString
+        val geoms = Demo.server.get(io.LoadGeoJson(geoJson)).par
+        val valid = geoms.filter(node => node.geom.isValid && node.geom.getGeometryType == "Polygon")
+        val tenPercent = Random.shuffle(valid.toList).take((valid.length * .10).toInt)
+     // val results = valid.flatMap {g =>
+     val results = tenPercent.flatMap {g =>
+       dates(sat)(year).map {date =>
           {
             val polygon = Polygon(g.geom,0)
             val coords = Demo.server.get(GetCentroid(polygon)).geom.getCoordinate
-            val tileSet = RasterSource(conf.store(),s"ltm5_${year}_${date}_clean")
+            val tileSet = RasterSource(conf.store(),s"ltm${sat}_${year}_${date}_clean")
             tileSet.zonalEnumerate(polygon).run match {
               case Complete(result, _) =>  (coords.y, coords.x, date, result)
               case _ => (coords.y, coords.x, date, Array.empty)
@@ -82,8 +93,8 @@ object RunMe {
       }
 
       import java.io.PrintWriter
-      val output = new PrintWriter(s"/home/ejc/$year$beetfile.txt")
-      output.println(heading(year))
+      val output = new PrintWriter(s"$outputPath/ltm${sat}_$year$beetfile.txt")
+      output.println(heading(sat)(year))
       val filtered = results.groupBy {
         case (a, b, _, _) => (a, b)
       }.mapValues(b => b.map(c => c._3 -> c._4).toList.sortBy(_._1)).toList.sortBy(_._1._1).seq
@@ -99,6 +110,7 @@ object RunMe {
       )
       output.close()
       println(s"Results length ${results.length}")
+      }
     }
     finally {
       Demo.server.shutdown()
