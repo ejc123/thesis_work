b5136732bef4452413a7d37fcfe04cf064ee8cd5
Eric J. Christeson <Eric.J.Christeson@gmail.com>
2013-12-31 11:42:25 -0600

    Try this out

--- src/main/scala/net/christeson/geotrellis/template/TemplateResource.scala
+++ src/main/scala/net/christeson/geotrellis/template/TemplateResource.scala
@@ -98,16 +98,16 @@ object RunMe {
       val output = new PrintWriter(s"$outputPath/ltm${sat}_$year$beetfile.txt")
       output.println(s"LENGTH,${heading(sat)(year)}")
       val filtered = results.groupBy {
-        case (a, b, _) => (a, b)
-      }.seq.mapValues(b => b.map(c => c._3).toList).toList.sortBy(_._1._1.x)
+        case (a, _, _) => a
+      }.seq.mapValues(b => b.map(c => Map(c._2 -> c._3)).seq).toList.sortBy(_._1.x)
       filtered.map(a => {
-        for( q <- 0 to a._2(1).length -1 ) {
-          if(a._2.foldLeft(false)((a,b) => isData(b(q)) || a)) {
+        for( q <- 0 to a._2.head.values.foldLeft(0)((a,b) => max(a,b.length)) - 1 ) {
+//          if(a._2.foldLeft(false)((a,b) => isData(b(q)) || a)) {
             output.print(s"${a._2.length},")
-            output.print(s"${a._1._1.x},${a._1._1.y}")
-            a._2.map(b => output.print(s""",${fetch(b(q))}"""))
+            output.print(s"${a._1.x},${a._1.y}")
+            a._2.map(c => output.print(s""",${fetch(c(q))}"""))
             output.println(s""","$beets"""")
-          }
+//          }
         }
       }
       )
@@ -121,4 +121,5 @@ object RunMe {
   }
 
   @inline final def fetch(v: Int): String = if(isData(v)) v.toString else ""
+  @inline final def max(a: Int, b: Int): Int = a > b ? a : b
 }
