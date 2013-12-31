5f41090c179a105eafa1ba4a9495f20b577d764f
Eric J. Christeson <Eric.J.Christeson@gmail.com>
2013-12-30 22:24:57 -0600

    Check out geojson files for weirdness

--- src/main/scala/net/christeson/geotrellis/template/TemplateResource.scala
+++ src/main/scala/net/christeson/geotrellis/template/TemplateResource.scala
@@ -16,24 +16,7 @@ object Demo {
 class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
   val start = opt[Int](required = true)
   val end = opt[Int](required = true)
-  val store = opt[String](default = Some("tiled"), required = true)
-  val sat = opt[Int](required = true, default=Some(5))
-  val prior = toggle(descrYes = "Use prior year for negative sample", descrNo = "Use next year for negative sample")
   val outputPath = opt[String](default = Some("/home/ejc"))
-
-  validate (sat) { a =>
-    if(a == 5 || a == 7) Right(Unit)
-    else Left("sat must be either 5 or 7")
-  }
-
-  validate (sat,start) { (sa,st) =>
-    sa match {
-      case 5 => if(st<=2011 && st >= 2007)  Right(Unit)
-        else Left("year must be between 2007 and 2011 inclusive for landsat5")
-      case 7 =>  if(st <=2013 && st >= 2008)  Right(Unit)
-        else Left("year must be between 2008 and 2013 inclusive for landsat7")
-    }
-  }
 }
 
 object RunMe {
@@ -43,64 +26,34 @@ object RunMe {
     val start = conf.start()
     val end = conf.end()
 
-    val prior = conf.prior() match {
-        case true => -1
-        case _ => 1
-    }
-    val outputPath = conf.outputPath()
-    val sat = conf.sat()
-
     import scalax.io.Resource
     implicit val codec = scalax.io.Codec.UTF8
 
     import scala.util.Random
 
     try {
-      for {
-        year <- start to end
-        feature_year <- (year + prior) match {
-          case a if (a < year) => year to a by -1
-          case a => year to a
-        }
-     } yield {
-        val beets = feature_year == year match {
-          case true => "beets"
-          case false => "nonbeets"
-        }
-        val beetfile = feature_year == year match {
-          case true => beets
-          case false => s"$beets$feature_year"
-        }
-        val featurePath = s"file:///home/ejc/geotrellis/data/${feature_year}_field_boundary_cropped.geojson"
+      for (year <- start to end) {
+        val featurePath = s"file:///home/ejc/geotrellis/data/${year}_field_boundary_cropped.geojson"
         val resource = Resource.fromURL(featurePath).chars
         val geoJson = resource.mkString
         val geoms = Demo.server.get(io.LoadGeoJson(geoJson)).par
-        val valid = geoms.filter(node => node.geom.isValid && node.geom.getGeometryType == "Polygon")
-        val tenPercent = Random.shuffle(valid.toList).take((valid.length * .10).toInt)
-     // val results = valid.flatMap {g =>
-     val results = tenPercent.flatMap {g =>
-       dates(sat)(year).map {date =>
-          {
+        // val valid = geoms.filter(node => node.geom.isValid && node.geom.getGeometryType == "Polygon")
+     val results = geoms.map {g => {
             val polygon = Polygon(g.geom,0)
+            val ind = g.data.get.get("IND").getTextValue
             val coords = Demo.server.get(GetCentroid(polygon)).geom.getCoordinate
-            val tileSet = RasterSource(conf.store(),s"ltm${sat}_${year}_${date}_clean")
-            tileSet.zonalEnumerate(polygon).run match {
-              case Complete(result, _) =>  (coords.y, coords.x, date, result)
-              case _ => (coords.y, coords.x, date, Array.empty)
-            }
-          }
+          (ind,coords)
          }
       }
 
-      import java.io.PrintWriter
+/*      import java.io.PrintWriter
       val output = new PrintWriter(s"$outputPath/ltm${sat}_$year$beetfile.txt")
       output.println(heading(sat)(year))
+*/
       val filtered = results.groupBy {
-        case (a, b, _, _) => (a, b)
-      }.mapValues(b => b.map(c => c._3 -> c._4).groupBy {
-        case(a,_) => a
-
-      }.mapValues(b => b.map(c => {for (j <- 0 to c._2.length) ).toList.sortBy(_._1))).toList.sortBy(_._1._1).seq
+  case (a,b) => (b)
+}.mapValues(b => b.map(c => c._1).toList).toList.filter(_._2.length > 1)
+ /*
       filtered.map(a => {
         for( q <- 0 to a._2(1)._2.length -1 ) {
           if(a._2.foldLeft(false)((a,b) => isData(b._2(q)) || a)) {
@@ -112,6 +65,8 @@ object RunMe {
       }
       )
       output.close()
+*/
+      filtered.foreach(a => println(s"Coords: ${a._1} INDs: ${a._2}"))
       println(s"Results length ${results.length}")
       }
     }
