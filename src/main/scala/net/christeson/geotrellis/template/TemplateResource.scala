c2d3fb32778832e6921e6322066386307b96a089
Eric J. Christeson <Eric.J.Christeson@gmail.com>
2014-03-10 22:21:20 -0500

    Make it compile

--- src/main/scala/net/christeson/geotrellis/template/TemplateResource.scala
+++ src/main/scala/net/christeson/geotrellis/template/TemplateResource.scala
@@ -80,7 +80,7 @@ object RunMe {
         val geoms = Demo.server.get(io.LoadGeoJson(geoJson)).par
         val valid = geoms.filter(node => node.geom.isValid && node.geom.getGeometryType == "Polygon")
         valid.tasksupport = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(4))
-        // val tenPercent = Random.shuffle(valid.toList).take((valid.length * .40).toInt)
+        val tenPercent = Random.shuffle(valid.toList).take((valid.length * .40).toInt)
         // val results = valid.flatMap { g =>
            val results = tenPercent.flatMap {g =>
           //  dates(sat)(year).map {
@@ -91,8 +91,8 @@ object RunMe {
                 val coords = Demo.server.get(GetCentroid(polygon)).geom.getCoordinate
                 val tileSet = RasterSource(conf.store(), s"${month}${year}_NDVI_TOA")
                 tileSet.zonalEnumerate(polygon).run match {
-                  case Complete(result, _) => (coords, date, result)
-                  case _ => (coords, date, Array.empty)
+                  case Complete(result, _) => (coords, month, result)
+                  case _ => (coords, month, Array.empty)
                 }
               }
             }
