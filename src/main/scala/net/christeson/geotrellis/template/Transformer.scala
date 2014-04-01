b7fb50dfdd9e583c453ef53d427c87cccf97d330
Eric J. Christeson <Eric.J.Christeson@gmail.com>
2014-04-01 16:46:06 -0500

    Clean up warnings

--- src/main/scala/net/christeson/geotrellis/template/Transformer.scala
+++ src/main/scala/net/christeson/geotrellis/template/Transformer.scala
@@ -4,13 +4,6 @@ import geotrellis.feature._
 import org.geotools.geometry.jts.JTS
 import org.geotools.referencing.CRS
 import org.opengis.referencing.crs.{CoordinateReferenceSystem => Crs}
-import org.opengis.referencing.operation.MathTransform
-
-import scala.collection.mutable
-
-import com.vividsolutions.jts.{ geom => jts }
-
-
 
 object Transformer {
   def transform[D](feature:Geometry[D],fromCRS:Crs,toCRS:Crs):Geometry[D] = {
